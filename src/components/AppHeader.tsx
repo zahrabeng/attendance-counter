d95fef80ff28459432cade05f9929a56747ec7a3
Zahra <bghzahra@outlook.com>
2022-03-10 22:16:30 +0000

    added && conditional rendering to app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,18 +3,13 @@ interface AppHeaderProps {
 }
 
 export default function AppHeader({ title }: AppHeaderProps): JSX.Element {
-  if (title.length >= 5) {
     return (
       <header>
         <h1>{title}</h1>
-      </header>
-    );
-  } else {
-    return (
-      <header>
-        <h1>{title}</h1>
-        <p>(The title is a work-in-progress)</p>
-      </header>
-    );
-  }
+     
+      {title.length < 5 && 
+      <p>(The title is a work-in-progress)</p>}
+    </header>
+    )
 }
+
